d728924bcfe99da05664f8119ea266cf02a6d631
rahul <4r7h2000rj@gmail.com>
2026-04-14 13:59:24 +0530

    Deployment corrections

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { polarClient } from "@polar-sh/better-auth"
 import { createAuthClient} from "better-auth/react"
 
 export const authClient  = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
     plugins: [polarClient()]
 })
